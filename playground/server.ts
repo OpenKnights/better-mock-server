c553f82d38f9357f5505ee4e5cfbaadd81433e5f
King-3 <75344974+coderking3@users.noreply.github.com>
2025-11-17 03:20:58 +0000

    🚀 feat: update playground server usage

--- playground/server.ts
+++ playground/server.ts
@@ -1,31 +1,33 @@
 /* eslint-disable no-console */
-import { createAppServer } from '../src/server'
+import { createAppServer, defineRoutes } from '../src'
 
-const appServer = createAppServer({
-  routes: {
-    '/': {
-      GET: () => {
-        return 'home'
-      }
-    },
-    '/all': (event) => {
-      console.log(`🚀 ~ event:`, event)
-      return 'all'
-    },
-    '/hello': {
-      GET: () => {
-        return 'hello world'
-      },
-      POST: {
-        handler: () => {
-          return 'aaa'
-        },
-        options: {
-          meta: { name: '333' }
-        }
-      }
+const routes = defineRoutes({
+  '/': {
+    GET: () => {
+      return 'home'
     }
   },
+  '/all': (event: any) => {
+    console.log(`🚀 ~ event:`, event)
+    return 'all'
+  },
+  '/hello': {
+    GET: () => {
+      return 'hello world'
+    },
+    POST: {
+      handler: () => {
+        return 'aaa'
+      },
+      options: {
+        meta: { name: '333' }
+      }
+    }
+  }
+})
+
+const appServer = createAppServer({
+  routes,
   port: 3060
 })
 console.log(`🚀 ~ appServer:`, appServer)
