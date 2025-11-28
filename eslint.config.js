684c66f1264b2070e636b3d35150c0607fcef771
king3 <w2196592083@gmail.com>
2025-11-28 08:58:38 +0000

    🔧 chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from '@king-3/eslint-config'
 
-export default defineConfig({
-  typescript: true
-})
+export default defineConfig(
+  {
+    typescript: true
+  },
+  {
+    files: ['**/*.md'],
+    rules: {
+      'import/first': 'off'
+    }
+  }
+)
